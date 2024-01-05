77d86b9fd8507bf90ea893e5980c9e5be99adc21
Habib-0007 <adebayohabib7@gmail.com>
2024-01-05 07:35:04 +0100

    made some more new changes

--- script.ts
+++ script.ts
@@ -5,4 +5,22 @@ var displayBox = document.querySelector(".image-display");
 genBtn.addEventListener("click", () => {
 	imgDesValue = imgDes.value;
 	displayBox.innerHTML = "<section class='spinner'><div></div></section>";
+
+	var { Hercai, QuestionData, DrawImageData } = require("hercai");
+
+	const herc = new Hercai(); //new Hercai("your api key"); => Optional
+
+	/* Question Example For TypeScript */
+	herc
+		.question({ model: "v3-beta", content: "hi, how are you?" })
+		.then((response: typeof QuestionData) => {
+			console.log(response.reply);
+		});
+
+	/* DrawImage Example For TypeScript */
+	herc
+		.drawImage({ model: "v2", prompt: "anime girl" })
+		.then((response: typeof DrawImageData) => {
+			console.log(response.url);
+		});
 });
