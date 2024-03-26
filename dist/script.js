bc856270047494d6e49fc0b7d64fde90fdc9b505
Habib-0007 <adebayohabib7@gmail.com>
2024-03-26 14:45:10 +0100

    updated the file structure

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hercai_1 = require("hercai");
-var imgDes = document.querySelector(".form input");
-var genBtn = document.querySelector(".form button");
-var displayBox = document.querySelector(".image-display");
-genBtn.addEventListener("click", function () {
-    imgDesValue = imgDes.value;
-    displayBox.innerHTML = "<section class='spinner'><div></div></section>";
-    var url = "https://hercai.onrender.com/v3/text2image?prompt=" + imgDesValue;
-    console.log(url);
-    /* 	fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            displayBox.innerHTML = "Boy";
-        }); */
-    var herc = new hercai_1.Hercai();
-    herc
-        .drawImage({ model: "v3", prompt: "anime girl", negative_prompt: "" })
-        .then(function (response) {
-        console.log(response.url);
-    });
-});
+var imgDes = document.querySelector(
+	".form input"
+);
+var genBtn = document.querySelector(
+	".form button"
+);
+var displayBox = document.querySelector(
+	".image-display"
+);
+
+document.body.style.height = `${window.innerHeight}px`;
+
+genBtn.addEventListener(
+	"click",
+	function () {
+		imgDesValue = imgDes.value;
+		displayBox.innerHTML =
+			"<section class='spinner'><div></div></section>";
+
+		fetch(
+			"https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+			{
+				method: "POST",
+				headers: {
+					"content-type":
+						"application/json",
+				},
+				body: JSON.stringify({
+					inputs: imgDesValue,
+				}),
+			}
+		)
+			.then(res => res.blob())
+			.then(blob => {
+				console.log(blob);
+				displayBox.innerHTML = "";
+				const img =
+					document.createElement("img");
+				img.src =
+					window.URL.createObjectURL(
+						blob
+					);
+				displayBox.appendChild(img);
+			});
+	}
+);
